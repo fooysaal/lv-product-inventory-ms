fbe90ee8c83bc854bed0e2476e45d19162390330
Foysal <mohibullah2k0@gmail.com>
2025-12-07 12:56:31 +0600

    Api & Vue installation

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<!-- resources/js/App.vue -->
+<template>
+    <div>
+        <h1>Hello from Vue!</h1>
+        <!-- Your other Vue components will go here -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    // Component logic
+};
+</script>
+
+<style scoped>
+/* Scoped styles for this component */
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue'; // main Vue component
+
+createApp(App).mount('#app'); // Mount the Vue app to an element with id="app"
